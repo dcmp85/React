d73b2af38491d439bcd6626292cb745e95e790e0
David Campbell <djcampbe@gmail.com>
2018-07-02 12:46:04 -0500

    Author Quiz Init

---
+++ Test/authorquiz/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AuthorQuiz from './AuthorQuiz';
+import registerServiceWorker from './registerServiceWorker';
+
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'images/authors/marktwain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Adventures of Huckleberry Finn']
+    }
+];
+
+const state = {
+    turnData: {
+        author: authors[0],
+        books: authors[0].books
+    }
+}
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
+registerServiceWorker();
